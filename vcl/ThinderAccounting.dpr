40388e05e75db8264fe8f125aa13119ad372aae9
Thorsten Hindermann <hindermath@googlemail.com>
2023-12-29 16:37:27 +0100

    Added empty VCL desktop app

---
+++ vcl/ThinderAccounting.dpr
@@ -0,0 +1,14 @@
+program ThinderAccounting;
+
+uses
+  Vcl.Forms,
+  UFrmMain in 'src\forms\UFrmMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
