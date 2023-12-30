2ee5835d954c8448ad105006b6b43bbf8d6951ec
Thorsten Hindermann <hindermath@googlemail.com>
2023-12-30 13:40:08 +0100

    Added to example DB connections
    Added two TMS Aurelius connections, first to  my local MSSQLServer with dedicated test-user and database, second to a local SQLite database. All data will be test data and have no connection to real persons or companies. Keep that in mind.

--- vcl/src/forms/UFrmMain.pas
+++ vcl/src/forms/UFrmMain.pas
@@ -11,11 +11,20 @@ uses
   Vcl.Graphics,
   Vcl.Controls,
   Vcl.Forms,
-  Vcl.Dialogs
+  Vcl.Dialogs, Aurelius.Drivers.MSSQL, Aurelius.Drivers.SQLite,
+  FireDAC.Stan.ExprFuncs, FireDAC.Phys.SQLiteWrapper.Stat,
+  FireDAC.Phys.SQLiteDef, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.VCLUI.Wait,
+  Data.DB, FireDAC.Comp.Client, FireDAC.Phys.SQLite, Aurelius.Comp.Connection
   ;
 
 type
   TForm1 = class(TForm)
+    Connection: TAureliusConnection;
+    MemConnection: TAureliusConnection;
+    SQLiteUnits: TFDPhysSQLiteDriverLink;
+    FDConnection: TFDConnection;
   private
     { Private-Deklarationen }
   public
