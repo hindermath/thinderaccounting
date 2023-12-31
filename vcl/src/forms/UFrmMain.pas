2427ea15582c6d128c4adb5788aa69c3fa2f80e9
Thorsten Hindermann <hindermath@googlemail.com>
2023-12-31 18:42:42 +0100

    Drop 64 bit target and a quick model test
    Droping the 64 bit target and switch to 32 bit because the BPL wish to be a 32 bit target. A quick test of the components with the TDocument I made a quick test with Data components described in chapters 4.5.3 and 4.5.4. Look good... no compilation and runtime error and I see the columns in the grid.

--- vcl/src/forms/UFrmMain.pas
+++ vcl/src/forms/UFrmMain.pas
@@ -3,42 +3,51 @@ unit UFrmMain;
 interface
 
 uses
-  Winapi.Windows,
-  Winapi.Messages,
+  Winapi.Windows
+ ,Winapi.Messages
 
-  System.SysUtils,
-  System.Variants,
-  System.Classes,
-  System.Generics.Collections,
+ ,System.SysUtils
+ ,System.Variants
+ ,System.Classes
+ ,System.Generics.Collections
 
-  Bcl.Types.Nullable,
-  Data.DB,
+ ,Bcl.Types.Nullable
+ ,Data.DB
 
-  Vcl.Graphics,
-  Vcl.Controls,
-  Vcl.Forms,
-  Vcl.Dialogs,
+ ,Vcl.Graphics
+ ,Vcl.Controls
+ ,Vcl.Forms
+ ,Vcl.Dialogs
 
-  FireDAC.Stan.ExprFuncs,
-  FireDAC.Phys.SQLiteWrapper.Stat,
-  FireDAC.Phys.SQLiteDef,
-  FireDAC.Stan.Intf,
-  FireDAC.Stan.Option,
-  FireDAC.Stan.Error,
-  FireDAC.UI.Intf,
-  FireDAC.Phys.Intf,
-  FireDAC.Stan.Def,
-  FireDAC.Stan.Pool,
-  FireDAC.Stan.Async,
-  FireDAC.Phys,
-  FireDAC.VCLUI.Wait,
-  FireDAC.Comp.Client,
-  FireDAC.Phys.SQLite
+ ,FireDAC.Stan.ExprFuncs
+ ,FireDAC.Phys.SQLiteWrapper.Stat
+ ,FireDAC.Phys.SQLiteDef
+ ,FireDAC.Stan.Intf
+ ,FireDAC.Stan.Option
+ ,FireDAC.Stan.Error
+ ,FireDAC.UI.Intf
+ ,FireDAC.Phys.Intf
+ ,FireDAC.Stan.Def
+ ,FireDAC.Stan.Pool
+ ,FireDAC.Stan.Async
+ ,FireDAC.Phys
+ ,FireDAC.VCLUI.Wait
+ ,FireDAC.Comp.Client
+ ,FireDAC.Phys.SQLite, Aurelius.Bind.BaseDataset, Aurelius.Bind.Dataset,
+  Vcl.Grids, Vcl.DBGrids
 
   ;
 
 type
   TForm1 = class(TForm)
+    Documents: TAureliusDataset;
+    DocumentsSelf: TAureliusEntityField;
+    DocumentsId: TIntegerField;
+    DocumentsDocument: TBlobField;
+    DocumentsOriginalFilename: TStringField;
+    DocumentsKeyFilename: TStringField;
+    sourceDocuments: TDataSource;
+    DocumentGrid: TDBGrid;
   private
     { Private-Deklarationen }
   public
