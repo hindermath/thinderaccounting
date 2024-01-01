c6b477802577c7d537d68b89db342975cd76928f
Thorsten Hindermann <hindermath@googlemail.com>
2024-01-01 15:17:47 +0100

    EyeCandy of the uses clause

--- vcl/src/forms/UFrmMain.pas
+++ vcl/src/forms/UFrmMain.pas
@@ -3,38 +3,42 @@ unit UFrmMain;
 interface
 
 uses
-  Winapi.Windows
- ,Winapi.Messages
+   Winapi.Windows
+ , Winapi.Messages
 
- ,System.SysUtils
- ,System.Variants
- ,System.Classes
- ,System.Generics.Collections
+ , System.SysUtils
+ , System.Variants
+ , System.Classes
+ , System.Generics.Collections
 
- ,Bcl.Types.Nullable
- ,Data.DB
+ , Bcl.Types.Nullable
+ , Data.DB
 
- ,Vcl.Graphics
- ,Vcl.Controls
- ,Vcl.Forms
- ,Vcl.Dialogs
+ , Vcl.Graphics
+ , Vcl.Controls
+ , Vcl.Forms
+ , Vcl.Dialogs
+ , Vcl.Grids
+ , Vcl.DBGrids
 
- ,FireDAC.Stan.ExprFuncs
- ,FireDAC.Phys.SQLiteWrapper.Stat
- ,FireDAC.Phys.SQLiteDef
- ,FireDAC.Stan.Intf
- ,FireDAC.Stan.Option
- ,FireDAC.Stan.Error
- ,FireDAC.UI.Intf
- ,FireDAC.Phys.Intf
- ,FireDAC.Stan.Def
- ,FireDAC.Stan.Pool
- ,FireDAC.Stan.Async
- ,FireDAC.Phys
- ,FireDAC.VCLUI.Wait
- ,FireDAC.Comp.Client
- ,FireDAC.Phys.SQLite, Aurelius.Bind.BaseDataset, Aurelius.Bind.Dataset,
-  Vcl.Grids, Vcl.DBGrids
+ , FireDAC.Stan.ExprFuncs
+ , FireDAC.Phys.SQLiteWrapper.Stat
+ , FireDAC.Phys.SQLiteDef
+ , FireDAC.Stan.Intf
+ , FireDAC.Stan.Option
+ , FireDAC.Stan.Error
+ , FireDAC.UI.Intf
+ , FireDAC.Phys.Intf
+ , FireDAC.Stan.Def
+ , FireDAC.Stan.Pool
+ , FireDAC.Stan.Async
+ , FireDAC.Phys
+ , FireDAC.VCLUI.Wait
+ , FireDAC.Comp.Client
+ , FireDAC.Phys.SQLite
+
+ , Aurelius.Bind.BaseDataset
+ , Aurelius.Bind.Dataset
 
   ;
 
