7197920364ad7343c63e6b2d6d8d663d8d0a307c
Thorsten Hindermann <hindermath@googlemail.com>
2024-01-07 23:03:21 +0100

    Beginning class implementation

--- vcl/src/forms/UFrmBase.pas
+++ vcl/src/forms/UFrmBase.pas
@@ -3,13 +3,32 @@ unit UFrmBase;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+    Winapi.Windows
+  , Winapi.Messages
+
+  , System.SysUtils
+  , System.Variants
+  , System.Classes
+
+  , Vcl.Graphics
+  , Vcl.Controls
+  , Vcl.Forms
+  , Vcl.Dialogs
+
+  , UDataManager
+
+  ;
 
 type
   TFrmBase = class(TForm)
+    constructor FormCreate(Sender: TObject);
+    destructor FormDestroy(Sender: TObject);
   private
+    FOwnsObjectManager: Boolean;
+    FStoreControls: Boolean;
     { Private-Deklarationen }
+  protected
+    FDataManager: TDataManager;
   public
     { Public-Deklarationen }
   end;
@@ -20,5 +39,17 @@ var
 implementation
 
 {$R *.dfm}
+
+constructor TFrmBase.FormCreate(Sender: TObject);
+begin
+  inherited;
+  // TODO -cMM: TFrmBase.FormCreate default body inserted
+end;
+
+destructor TFrmBase.FormDestroy(Sender: TObject);
+begin
+  inherited;
+  // TODO -cMM: TFrmBase.FormDestroy default body inserted
+end;
 
 end.
